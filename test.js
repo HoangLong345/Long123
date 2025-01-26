8445fb5c0bb5f8b259da3067050e8d99551977dc
HoangLong345 <longhoangpk44@gmail.com>
2025-01-26 20:57:05 +0700

    them file moi

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 // khởi tạo dự án playwright
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
 (async () => {
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('https://www.google.com');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto("https://www.google.com");
+  await page.screenshot({ path: "example.png" });
   await browser.close();
 })();
 // chạy file test.js
